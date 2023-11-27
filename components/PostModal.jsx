852f96563ea0a3e9c6bbbf4117592131720ee066
dd3dd <jacky81810@gmail.com>
2023-11-28 00:55:08 +0800

    dfdd

--- components/PostModal.jsx
+++ components/PostModal.jsx
@@ -43,7 +43,7 @@ export default function PostModal({ children }) {
                 ref={wrapper}
                 className="overflow-auto  absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-modalWidth h-screen bg-white "
             >
-                <button ref={backbtn}>
+                <button className='m-12' ref={backbtn}>
                     x
                 </button>
                 {children}
