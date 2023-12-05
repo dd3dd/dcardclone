e4de5a724a6472165a9e9d460e3890a3e2615cff
dd3dd <jacky81810@gmail.com>
2023-12-05 16:54:19 +0800

    ffff

--- app/f/page.jsx
+++ app/f/page.jsx
@@ -4,8 +4,8 @@ import SideBar from "@/components/SideBar";
 export default function Page() {
     return (
         <div className="flex max-w-7xl mx-auto">
-            <SideBar />
-            <Container />
+            {/* <SideBar />
+            <Container /> */}
         </div>
     )
 }
