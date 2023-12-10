e7c6afc9540c5b3b0eb2205d9c2fb301d1608092
dd3dd <jacky81810@gmail.com>
2023-12-10 21:37:19 +0800

    ddd

--- app/page.jsx
+++ app/page.jsx
@@ -7,10 +7,10 @@ export default async function Home() {
   if (session) redirect("/f");
   return (
     <div className=" relative flex justify-center items-center">
-      {/* < div className='w-full max-w-boardModalWidth flex flex-col items-center my-8 bg-white rounded-xl' >
+      < div className='w-full max-w-boardModalWidth flex flex-col items-center my-8 bg-white rounded-xl' >
         <p className='text-3xl mt-8 mb-4'>登入</p>
         <LoginForm />
-      </div > */}
+      </div >
     </div>
   )
 }
